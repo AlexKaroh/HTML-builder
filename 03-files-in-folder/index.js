0f1fa261a738dd730890e209f4f445c519b745e1
AlexKaroh <106170332+AlexKaroh@users.noreply.github.com>
2022-10-31 13:23:53 +0400

    feat: solve 03-files-in-folder task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -6,7 +6,7 @@ fs.readdir(path.join(__dirname,'secret-folder'), {withFileTypes: true}, (err, fi
     
     files.forEach(file => {
         if(file.isFile()) 
-        fs.stat(path.join(__dirname,'secret-folder', file.name), (error, stats) => {
+        fs.stat(path.join(__dirname,'secret-folder', file.name), (err, stats) => {
             if (err) {
                 throw err;
             }
